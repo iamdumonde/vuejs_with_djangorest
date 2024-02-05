ad47b094cc4fd7cc88736ccf8aab6fa8cb27caa7
iamdumonde <martinaboumonde@gmail.com>
2024-02-05 15:13:53 +0100

    api postmann test

--- django_rest_api_tasks/tasks/urls.py
+++ django_rest_api_tasks/tasks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from tasks import views
 
 #define the urls
-urlpatters = [
+urlpatterns = [
     path('tasks/', views.tasks),
     path('tasks/<int:pk>/', views.task_detail),
 ]
