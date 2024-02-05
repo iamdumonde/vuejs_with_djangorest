cd9cbca4a2bc368123a7809bbc8e22a287f000e7
iamdumonde <martinaboumonde@gmail.com>
2024-02-05 18:12:13 +0100

    settings.py

--- django_rest_api_tasks/django_rest_api_tasks/settings.py
+++ django_rest_api_tasks/django_rest_api_tasks/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'tasks',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
